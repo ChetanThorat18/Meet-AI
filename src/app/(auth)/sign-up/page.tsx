59d776489ea631c612ce7a191cd991078d181df4
ChetanThorat18 <chetanthorat146@gmail.com>
2025-06-29 17:07:59 +0530

    04: Authentication UI

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/ui/card";
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+const SignUpPage = () => {
+  return <SignUpView />
+
+};
+
+export default SignUpPage;
